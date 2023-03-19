569fb90749cf69045bc0539e1dafdd7d13727f6c
Jeff Machyo <jeffmachyo@gmail.com>
2023-03-19 12:07:49 -0400

    Added Testing example

--- installer.sh
+++ installer.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-# export PYTHONPATH="/var/lib/jenkins/workspace/Home Automation Python Build And Test/src/main/python:/var/lib/jenkins/workspace/Home Automation Python Build And Test/src/test/python:$PYTHONPATH"
+export PYTHONPATH="/var/lib/jenkins/workspace/Home Automation Python Build And Test/src/main/python:/var/lib/jenkins/workspace/Home Automation Python Build And Test/src/test/python:$PYTHONPATH"
+
 # rm -rf out/build CMakelists.txt
 # cmake -S . -B out/build
 # make -C out/build
+python3 src/test/python/programmingtheiot/part01/unit/system/SystemCpuUtilTaskTest.py
 echo $PYTHONPATH
